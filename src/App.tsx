be803a0edca802673b5e6da9cbc11f99c27bb01a
NotSuzpicious <rishi.17867@sakec.ac.in>
2026-08-18 23:10:05 +0530

    Set up Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1>Chessroom</h1>
+    <div className="min-h-screen bg-[#11110f] flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-[#f1ede3]">
+        Chessroom
+      </h1>
     </div>
   )
 }
